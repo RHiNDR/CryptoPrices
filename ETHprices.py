83c3180427b29c201d42469771592832a3ddac7c
RHiNDR <83766065+RHiNDR@users.noreply.github.com>
2024-03-09 20:44:46 +1100

    Add files via upload

---
+++ ETHprices.py
@@ -0,0 +1,129 @@
+import requests
+
+combined_data = {}
+
+def IR():
+    url = "https://api.independentreserve.com/Public/GetMarketSummary"
+    params = {
+        "primaryCurrencyCode": "eth",
+        "secondaryCurrencyCode": "aud"
+    }
+
+    headers = {
+        "Accept-Encoding": "gzip, deflate"
+    }
+
+    try:
+        response = requests.get(url, params=params, headers=headers)
+        
+        # Check if the request was successful (status code 200)
+        if response.status_code == 200:
+            # You can access the response data in JSON format using response.json()
+            data = response.json()
+            last_price = round(data.get("LastPrice"), 2)
+            if last_price is not None:
+                # Create a dictionary to store the data
+                global combined_data
+                combined_data["Independent Reserve"] = last_price
+                combined_data["Independent Reserve"] = [combined_data["Independent Reserve"]]
+                combined_data.setdefault("Independent Reserve", []).append("https://portal.independentreserve.com/invite/NCWJQC")
+                # print(combined_data)
+            else:
+                print("Last Price not found in response.")
+        else:
+            print("HTTP Error:", response.status_code)
+            print("Error Message:", response.text)
+    except Exception as e:
+        print("Exception occurred:", str(e))
+
+def BTCM():
+    url = "https://api.btcmarkets.net/v3/markets/ETH-AUD/ticker"
+
+    try:
+        response = requests.get(url)
+        
+        # Check if the request was successful (status code 200)
+        if response.status_code == 200:
+            # You can access the response data in JSON format using response.json()
+            data = response.json()
+            last_price = float(data.get("lastPrice"))
+            round(last_price, 2)
+            if last_price is not None:
+                # Create a dictionary to store the data
+                global combined_data
+                combined_data["BTC Markets"] = last_price
+                combined_data["BTC Markets"] = [combined_data["BTC Markets"]]
+                combined_data.setdefault("BTC Markets", []).append("https://www.btcmarkets.net/")
+                #print(combined_data)
+            else:
+                print("Last Price not found in response.")
+        else:
+            print("HTTP Error:", response.status_code)
+            print("Error Message:", response.text)
+    except Exception as e:
+        print("Exception occurred:", str(e))
+
+def CS():
+    url = "https://www.coinspot.com.au/pubapi/v2/latest"
+
+    try:
+        response = requests.get(url)
+        
+        # Check if the request was successful (status code 200)
+        if response.status_code == 200:
+            # You can access the response data in JSON format using response.json()
+            data = response.json()
+            # Extract the "btc" data
+            btc_data = data["prices"]["eth"]
+            # Extract the "last" value
+            last_price = float(btc_data["last"])
+            round(last_price, 2)
+            if last_price is not None:
+                # Create a dictionary to store the data
+                global combined_data
+                combined_data["CoinSpot"] = last_price
+                combined_data["CoinSpot"] = [combined_data["CoinSpot"]]
+                combined_data.setdefault("CoinSpot", []).append("https://www.coinspot.com.au/join/REFMH5CEM")
+                #print(combined_data)
+            else:
+                print("Last Price not found in response.")
+        else:
+            print("HTTP Error:", response.status_code)
+            print("Error Message:", response.text)
+    except Exception as e:
+        print("Exception occurred:", str(e))
+
+def CT():
+    url = "https://trade.cointree.com/api/prices/AUD/change/24h?symbols=ETH"
+
+    try:
+        response = requests.get(url)
+        
+        # Check if the request was successful (status code 200)
+        if response.status_code == 200:
+            # You can access the response data in JSON format using response.json()
+            data = response.json()
+            last_price = round(data[0].get("spot"), 2)
+            if last_price is not None:
+                # Create a dictionary to store the data
+                global combined_data
+                combined_data["Cointree"] = last_price
+                combined_data["Cointree"] = [combined_data["Cointree"]]
+                combined_data.setdefault("Cointree", []).append("https://www.cointree.com?r=jLg8WjR65qIg")
+                #print(combined_data)
+            else:
+                print("Last Price not found in response.")
+        else:
+            print("HTTP Error:", response.status_code)
+            print("Error Message:", response.text)
+    except Exception as e:
+        print("Exception occurred:", str(e))
+
+def ETH():
+    IR()
+    BTCM()
+    CS()
+    CT()
+    global sorted_data
+    sorted_data = dict(sorted(combined_data.items(), key=lambda item: item[1]))
+    return [sorted_data]
--- my_app.py
+++ my_app.py
@@ -2,8 +2,17 @@ import streamlit as st
 from streamlit_autorefresh import st_autorefresh
 
 from BTCprices import BTC
+from ETHprices import ETH
+from USDTprices import USDT
 
 def main():
+    bitcoin()
+    ethereum()
+    usdt()
+    st_autorefresh(interval=10000)
+
+
+def bitcoin():
     # Assuming data is your list of dictionaries
     data = BTC()
     inner_dict = data[0]
@@ -16,8 +25,38 @@ def main():
         st.markdown("<h1 style='text-align: center; color: black;'>BTC AUD PRICES</h1>", unsafe_allow_html=True)
         for i, key in enumerate(keys):
             st.link_button(key + ": $" + str((values_list[i][0])), str(values_list[i][1]), use_container_width=True)
-        st.link_button("Spread: $" + str(round((values_list[3][0])-(values_list[0][0]), 2)), '', use_container_width=True)
-    st_autorefresh(interval=10000)
+        st.link_button("Spread: $" + str(round((values_list[3][0])-(values_list[0][0]), 2)), 'https://ozcryptoprices.streamlit.app/', use_container_width=True)
+
+def ethereum():
+    # Assuming data is your list of dictionaries
+    data = ETH()
+    inner_dict = data[0]
+
+    keys = inner_dict.keys()
+    values = inner_dict.values()
+    values_list = list(values)
+
+    with st.container(height=400):
+        st.markdown("<h1 style='text-align: center; color: black;'>ETH AUD PRICES</h1>", unsafe_allow_html=True)
+        for i, key in enumerate(keys):
+            st.link_button(key + ": $" + str((values_list[i][0])), str(values_list[i][1]), use_container_width=True)
+        st.link_button("Spread: $" + str(round((values_list[3][0])-(values_list[0][0]), 2)), 'https://ozcryptoprices.streamlit.app/', use_container_width=True)
+
+def usdt():
+    # Assuming data is your list of dictionaries
+    data = USDT()
+    inner_dict = data[0]
+
+    keys = inner_dict.keys()
+    values = inner_dict.values()
+    values_list = list(values)
+
+    with st.container(height=400):
+        st.markdown("<h1 style='text-align: center; color: black;'>USDT AUD PRICES</h1>", unsafe_allow_html=True)
+        for i, key in enumerate(keys):
+            st.link_button(key + ": $" + str((values_list[i][0])), str(values_list[i][1]), use_container_width=True)
+        st.link_button("Spread: $" + str(round((values_list[3][0])-(values_list[0][0]), 2)), 'https://ozcryptoprices.streamlit.app/', use_container_width=True)
+
 
 if __name__ == "__main__":
     main()
